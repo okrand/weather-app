09dc4e0c9f25e32827c6a7b90a7c97c636c305d5
Orkun Krand <ok250077@almgit.ncr.com>
2020-03-26 17:41:44 -0400

    Move weather component to its own file

---
+++ src/Weather.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react"
+import getLocation, { LocationResult } from "./location-api"
+import getWeatherFromZip, { WeatherResponse } from "./weather-api"
+
+type LocationProps = {
+  userCity: string
+}
+
+export const kToF = (kelvin: number) => ((kelvin - 273.15) * 1.8 + 32).toFixed()
+
+const Location = (userLocation: LocationProps) => {
+  const { userCity } = userLocation
+  return <p id="location">{userCity}</p>
+}
+
+const Weather = () => {
+  const [userCity, setLocation] = useState("")
+  const [weather, setWeather] = useState<string | undefined>(undefined)
+  const [weatherIconLink, setWeatherIconLink] = useState("")
+  useEffect(() => {
+    getLocation
+      .then((result: LocationResult) => {
+        const city = `${result.city}, ${result.state_code}`
+        setLocation(city)
+        return result
+      })
+      .then(result => getWeatherFromZip(result.postcode, result.country_code))
+      .then((newWeather: WeatherResponse) => {
+        newWeather && setWeather(kToF(newWeather.main.temp))
+        setWeatherIconLink(
+          `http://openweathermap.org/img/wn/${newWeather.weather[0].icon}@2x.png`
+        )
+      })
+  }, [])
+  return weather ? (
+    <div>
+      <img src={weatherIconLink} alt="weather-icon" />
+      <p id="weather">{weather && `${weather}°F`}</p>
+      <Location userCity={userCity} />
+    </div>
+  ) : (
+    <p id="loading">Loading!</p>
+  )
+}
+
+export default Weather
