c02d272770429cb2cfb664beb93ee9d3defd6cf3
Orkun Krand <ok250077@almgit.ncr.com>
2020-04-16 11:25:57 -0400

    Updates

---
+++ precache-manifest.80b9252c8c72e88925e73e5fc2f9cffc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a82b1cde04b41db5c286d04a74d233c",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "90fdab7a4d978625e679",
+    "url": "/weather-app/static/css/main.a25bac32.chunk.css"
+  },
+  {
+    "revision": "e95d5a2bb53d8afc86dc",
+    "url": "/weather-app/static/js/2.9bae190c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.9bae190c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90fdab7a4d978625e679",
+    "url": "/weather-app/static/js/main.3b230460.chunk.js"
+  },
+  {
+    "revision": "55676f7ecad5f9b6942e",
+    "url": "/weather-app/static/js/runtime-main.58bc7688.js"
+  }
+]);
